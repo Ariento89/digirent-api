2e5bbb19115f2d9c5ea0d61102825b4c6665b5d9
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2020-09-30 17:30:16 +0100

    define application methods to update user profile, bank detail and password

--- src/digirent/app/__init__.py
+++ src/digirent/app/__init__.py
@@ -1,10 +1,14 @@
 from datetime import date, datetime
 from typing import Optional, Union
+from uuid import UUID, uuid4
 from jwt import PyJWTError
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.session import Session
+
+from digirent.database.enums import Gender
 from .base import ApplicationBase
 from .error import ApplicationError
-from digirent.database.models import User, UserRole
+from digirent.database.models import BankDetail, User, UserRole
 
 
 class Application(ApplicationBase):
@@ -94,3 +98,59 @@ class Application(ApplicationBase):
             return user
         except PyJWTError:
             raise ApplicationError("Invalid token")
+
+    def update_profile(
+        self,
+        session: Session,
+        user: User,
+        first_name: str = None,
+        last_name: str = None,
+        city: str = None,
+        phone_number: str = None,
+        email: str = None,
+        dob: date = None,
+        gender: Gender = None,
+    ):
+        try:
+            result = self.user_service.update_user(
+                session,
+                user.id,
+                first_name=first_name,
+                last_name=last_name,
+                city=city,
+                phone_number=phone_number,
+                email=email,
+                gender=gender,
+                dob=dob,
+            )
+            if not result:
+                raise ApplicationError(f"User not found")
+        except IntegrityError as e:
+            marker = "unique constraint failed:"
+            if marker in str(e).lower():
+                raise ApplicationError("Email or Phone Number already exists")
+            raise ApplicationError(
+                "Invalid data, ensure firstname, lastname, phonenumber, and email are not empty"
+            )
+
+    def set_bank_detail(
+        self, session: Session, user: User, account_name: str, account_number: str
+    ):
+        bank_detail = BankDetail(uuid4(), account_name, account_number)
+        result = self.user_service.update_user(
+            session, user.id, bank_detail=bank_detail
+        )
+        if not result:
+            raise ApplicationError(f"User not found")
+
+    def update_password(
+        self, session: Session, user: User, old_password: str, new_password: str
+    ):
+        if not self.user_service.password_is_match(old_password, user.hashed_password):
+            raise ApplicationError("Wrong password")
+        new_hashed_password = self.user_service.hash_password(new_password)
+        result = self.user_service.update_user(
+            session, user.id, hashed_password=new_hashed_password
+        )
+        if not result:
+            raise ApplicationError("User not found")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,16 @@
+from digirent.core.services.user import UserService
 from tests.test_user_service import user_data
-from tests.conftest import admin_create_data
+from tests.conftest import (
+    admin_create_data,
+    application,
+    user_create_data,
+    user_service,
+)
 from digirent.app.error import ApplicationError
 import pytest
 from digirent.app import Application
 from sqlalchemy.orm.session import Session
-from digirent.database.models import User, UserRole
+from digirent.database.models import Landlord, Tenant, User, UserRole
 
 
 def test_create_tenant(
@@ -41,3 +47,128 @@ def test_create_admin(
     assert not admin.dob
     assert admin.role == UserRole.ADMIN
     assert session.query(User).count() == 1
+
+
+@pytest.mark.parametrize(
+    "user",
+    [
+        "tenant",
+        "landlord",
+        "admin",
+    ],
+    indirect=True,
+)
+def test_update_user_profile(user: User, session: Session, application: Application):
+    prev_firstname = user.first_name
+    prev_lastname = user.last_name
+    prev_city = user.city
+    new_fname = "new updated fname"
+    new_lname = "new updated lname"
+    new_city = "new updated city"
+    assert prev_firstname != new_fname
+    assert prev_lastname != new_lname
+    assert prev_city != new_city
+    application.update_profile(
+        session,
+        user,
+        first_name=new_fname,
+        last_name=new_lname,
+        city=new_city,
+        email=user.email,
+        phone_number=user.phone_number,
+        gender=user.gender,
+        dob=user.dob,
+    )
+    xuser = session.query(User).get(user.id)
+    assert xuser.first_name == new_fname
+    assert xuser.last_name == new_lname
+    assert xuser.city == new_city
+    assert xuser.email == user.email
+    assert xuser.phone_number == user.phone_number
+    assert xuser.gender == user.gender
+    assert xuser.dob == user.dob
+
+
+def test_update_user_email_to_existing_email_fail(
+    tenant: Tenant, landlord: Landlord, session: Session, application: Application
+):
+    prev_email = tenant.email
+    assert prev_email != landlord.email
+    with pytest.raises(ApplicationError) as e:
+        application.update_profile(
+            session,
+            tenant,
+            email=landlord.email,
+            first_name=tenant.first_name,
+            last_name=tenant.last_name,
+            gender=tenant.gender,
+            phone_number=tenant.phone_number,
+            city=tenant.city,
+            dob=tenant.dob,
+        )
+
+
+def test_update_user_phone_number_to_existing_email_fail(
+    tenant: Tenant, landlord: Landlord, session: Session, application: Application
+):
+    prev_phone_number = tenant.phone_number
+    assert prev_phone_number != landlord.phone_number
+    with pytest.raises(ApplicationError) as e:
+        application.update_profile(
+            session,
+            tenant,
+            phone_number=landlord.phone_number,
+            first_name=tenant.first_name,
+            last_name=tenant.last_name,
+            gender=tenant.gender,
+            email=tenant.email,
+            city=tenant.city,
+            dob=tenant.dob,
+        )
+
+
+@pytest.mark.parametrize(
+    "user",
+    [
+        "tenant",
+        "landlord",
+        "admin",
+    ],
+    indirect=True,
+)
+def test_set_bank_detail(user: User, session: Session, application: Application):
+    assert not user.bank_detail
+    account_name = "Test Account Name"
+    account_number = "Test Account Number"
+    application.set_bank_detail(session, user, account_name, account_number)
+    xuser = session.query(User).get(user.id)
+    assert xuser.bank_detail
+    assert xuser.bank_detail.account_name == account_name
+    assert xuser.bank_detail.account_number == account_number
+    assert xuser.bank_detail.user_id == user.id
+
+
+@pytest.mark.parametrize(
+    "user, user_create_data",
+    [
+        ("tenant", "tenant_create_data"),
+        ("landlord", "landlord_create_data"),
+        ("admin", "admin_create_data"),
+    ],
+    indirect=True,
+)
+def test_update_password(
+    user: User,
+    user_create_data: dict,
+    user_service: UserService,
+    session: Session,
+    application: Application,
+):
+    old_password = user_create_data["password"]
+    new_password = "newuserpassword"
+    assert old_password != new_password
+    application.update_password(session, user, old_password, new_password)
+    assert user.hashed_password != old_password
+    assert user.hashed_password != new_password
+    assert not user_service.password_is_match(old_password, user.hashed_password)
+    assert user_service.password_is_match(new_password, user.hashed_password)
